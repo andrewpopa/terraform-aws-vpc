0430435bacf639b62b692fc30cb27ad0012390c0
Andrei Popa <andrei@hashicorp.com>
2019-09-26 00:12:52 +0200

    restructure the test. cleanup example directory

--- example/test/integration/default/state.rb
+++
@@ -1,7 +0,0 @@
-describe command('terraform state list') do
-  its('stdout') { 
-    should eq "module.vpc.data.aws_availability_zones.available\nmodule.vpc.aws_internet_gateway.tf_gw\nmodule.vpc.aws_main_route_table_association.tf_route_table\nmodule.vpc.aws_route_table.tf_routing_table\nmodule.vpc.aws_subnet.tf_subnet[0]\nmodule.vpc.aws_subnet.tf_subnet[1]\nmodule.vpc.aws_subnet.tf_subnet[2]\nmodule.vpc.aws_vpc.tf_vpc\n"
-  }
-  its('stderr') { should eq '' }
-  its('exit_status') { should eq 0 }
-end
