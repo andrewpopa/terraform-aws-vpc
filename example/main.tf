d3a12dd1b39920031a2bb58e6e69828f203a5c39
Andrei Popa <38042793+andrewpopa@users.noreply.github.com>
2019-09-26 21:39:59 +0200

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -1,13 +1,12 @@
 module "vpc" {
-  source = "https://github.com/andrewpopa/terraform-aws-vpc"
+  source = "github.com/andrewpopa/terraform-aws-vpc"
 
   # VPC
   cidr_block = "172.16.0.0/16"
+  vpc_subnets = ["172.16.10.0/24", "172.16.11.0/24", "172.16.12.0/24"] 
   vpc_tags = {
     vpc         = "my-aws-vpc"
     subnet      = "subnet"
     internet_gw = "my-internet-gateway"
   }
-  vpc_subnets = ["172.16.10.0/24", "172.16.11.0/24", "172.16.12.0/24"]
-  
 }
